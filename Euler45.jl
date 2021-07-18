a61d78527ce3cba9efca49388ff55be9183cf720
Harrison Oates <harrisonoates2@gmail.com>
2021-07-18 10:39:07 +1000

    Update Euler45.jl
    Solved!

--- Euler45.jl
+++ Euler45.jl
@@ -7,5 +7,31 @@ P(m) = div(m * (3m-1), 2)
 H(n) = n*(2n-1)
 # Equate these so we arrive at:
 # n(2n-1) == \frac{m(3m-1)}{2}
-# Completing the square yields (6m-1)^2 - 3(4n-1)^2 = -2
-# This expands to 36n^2 - 48m^2 - 12n + 24m = 0
+# 2 n^2 - n = (3 m^2)/2 - m/2
+# 4n^2 - 2n = 3m^2 - m
+function ispentagonal(n)
+    j = 1
+    M = 0
+    while M < n
+        M = P(j)
+        j += 1
+    end
+
+    if M == n
+        return true
+    else return false
+    end
+end
+
+i = 143 ::Int64
+result = 0
+while true
+    global i += 1
+    global result = H(i)
+    if ispentagonal(result) == true
+        break
+    end
+end
+
+Euler45 = result
+print(Euler45)
