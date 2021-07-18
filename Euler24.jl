aad278410e229ca78ab0a65148be27ace8fcb20e
Harrison Oates <harrisonoates2@gmail.com>
2021-07-18 10:49:21 +1000

    Create Euler24.jl

---
+++ Euler24.jl
@@ -0,0 +1,6 @@
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+# Let's cut this problem down a bit.
+# If we hold the first digit constant, at 0, we then have 9! permutations
+# 10! = 3628800
+#  9! =  362880
+# That means that the first digit cannot be zero
